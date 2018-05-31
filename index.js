54831cf6a48f05a00112e9cb8b31d232188e9bd7
Kevin W <rocwang0316@gmail.com>
2018-05-31 13:17:03 -0700

    Added winning balls and mega balls' scatter chart with Google charts library.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const MEGA_REGX = /<tr class.+?<td class="dates">(.+?)<\/td>.+?(\d+).+?(\d+).+?(\d+).+?(\d+).+?(\d+).+?(\d+).+?(\d+).+?<\/tr>/g; // Use to get all winnig number and date from the source data.
 const YEAR_REGX = /\/(\d+)$/; // Use to get year from a date string.
 
+let megaObject;
+
 function getEmptyBallsObject() {
   const balls = {};
   for (let i = 1; i <= 70; i++) balls[i] = 0;
@@ -73,11 +75,73 @@ function getMegaDataArray(megaData) {
   return result;
 }
 
+function drawCharts() {
+  const dataTableArray = [];
+  Object.keys(megaObject.all.balls).forEach(key => dataTableArray.push([key * 1, megaObject.all.balls[key], megaObject['2018'].balls[key]]));
+  const data = new google.visualization.DataTable();
+  data.addColumn('number', 'Balls number');
+  data.addColumn('number', 'All year');
+  data.addColumn('number', '2018');
+  data.addRows(dataTableArray);
+  const options = {
+    title: 'Mega balls winning number for all data',
+    // hAxis: {title: 'Balls number', minValue: 1, maxValue: 80},
+    // vAxis: {title: 'Winning amount', minValue: 0, maxValue: 100},
+    // legend: 'none',
+    series: {
+            0: {targetAxisIndex: 0},
+            1: {targetAxisIndex: 1}
+          },
+    vAxes: {
+      // Adds titles to each axis.
+      0: {title: 'All year'},
+      1: {title: '2018'}
+    }
+  };
+
+  const ballsChart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+
+  ballsChart.draw(data, options);
+
+  dataTableArray.length = 0;
+  Object.keys(megaObject.all.megaBall).forEach(key => dataTableArray.push([key * 1, megaObject.all.megaBall[key], megaObject['2018'].megaBall[key] ? megaObject['2018'].megaBall[key] : 0]));
+  // console.log(dataTableArray);
+  const megaData = new google.visualization.DataTable();
+  megaData.addColumn('number', 'MegaBall number');
+  megaData.addColumn('number', 'All year');
+  megaData.addColumn('number', '2018');
+  megaData.addRows(dataTableArray);
+  const megaOptions = {
+    title: 'Mega balls winning number for all data',
+    // hAxis: {title: 'Balls number', minValue: 1, maxValue: 80},
+    // vAxis: {title: 'Winning amount', minValue: 0, maxValue: 100},
+    // legend: 'none',
+    series: {
+            0: {targetAxisIndex: 0},
+            1: {targetAxisIndex: 1}
+          },
+    vAxes: {
+      // Adds titles to each axis.
+      0: {title: 'All year'},
+      1: {title: '2018'}
+    }
+  };
+
+  const megaBallsChart = new google.visualization.ScatterChart(document.getElementById('mega_chart_div'));
+
+  megaBallsChart.draw(megaData, megaOptions);
+
+
+}
+
 /**
  * If the Mage data has already been fetched and stored in the localStorage, analyze them directly.
  * Otherwise, fetch all data and store in the localStorage.
  */
 $(document).ready(() => {
-  const htmlText = generateHtmlText(analyze(getMegaDataArray(megaData)));
+  megaObject = analyze(getMegaDataArray(megaData));
+  const htmlText = generateHtmlText(megaObject);
   $('#showDiv').html(htmlText);
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawCharts);
 });
